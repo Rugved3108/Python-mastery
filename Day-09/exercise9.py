5ac21907ea9bb5d48db846efa81e96c07c8b71d3
Rugved <rugvedsutar31@gmail.com>
2026-08-18 23:52:53 +0530

    Add day 9 function

---
+++ Day-09/exercise9.py
@@ -0,0 +1,25 @@
+#exercise1 : greeting
+
+# def greet(name):
+#     print("Hello", name)
+
+# greet("Rugved")
+
+#exercise2: square
+
+# def square(number):
+#     return number ** 2
+
+# result = square(5)
+# print(result)
+
+#exercise3: even or odd
+
+def check_even(number):
+    if number % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+result = check_even(10)
+print(result)
